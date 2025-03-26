9a6a84ae1ce98d2cd4409f7911fe815620e633d5
Datla Rohit Varma <visaal1582892@gmail.com>
2025-03-26 12:18:12 +0530

    made some changes

--- src/lib/db.tsx
+++ src/lib/db.tsx
@@ -6,6 +6,7 @@ const pool = new Pool({
 
 export async function query<T>(text:string, params?: any[]):Promise<T[]> {
     const client = await pool.connect();
+    console.log(client);
 
     try{
         const res = await client.query(text, params);
