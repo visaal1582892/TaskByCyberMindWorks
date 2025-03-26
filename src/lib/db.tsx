ec90ad84d66ae1075656cf5bfa7dfad02ae1644c
Datla Rohit Varma <visaal1582892@gmail.com>
2025-03-26 12:21:32 +0530

    made some changes

--- src/lib/db.tsx
+++ src/lib/db.tsx
@@ -1,7 +1,7 @@
 import {Pool} from "pg";
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL_UNPOOLED,
 })
 
 export async function query<T>(text:string, params?: any[]):Promise<T[]> {
